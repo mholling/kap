3476bff52dda96eae167bc267a7c65be2922abc4
Matthew Hollingworth <mdholling@gmail.com>
2010-02-02 10:51:38 +1100

    improved efficiency of attitude estimation

--- attitude.cpp
+++ attitude.cpp
@@ -13,14 +13,21 @@ void Attitude::EstimateTask::run() {
     const Vector b1 = app.accelerometer.vector().normalised(); // gravity
     const Vector b2 = (b1 * app.magnetometer.vector()).normalised(); // magnetic west
     const Vector b3 = b1 * b2;
+    
+    // const Vector b1r1a1_plus_b2r2a2 = b1 * r1 * a1 + b2 * r2 * a2;
+    // const Vector b3r3 = b3 * r3;
+    // const Vector b3_plus_r3 = b3 + r3;
+    // const float b3_dot_r3_plus_1 = 1.0 + b3 % r3;
+    // const float alpha = b3_dot_r3_plus_1 * (b1 % r1 * a1 + b2 % r2 * a2) + b3r3 % b1r1a1_plus_b2r2a2;
+    // const float beta = b3_plus_r3 % b1r1a1_plus_b2r2a2;
+    // const float gamma = sqrt(alpha * alpha + beta * beta);
+
+    const Vector b1r1a1_plus_b2r2a2(a2 * b2.z - a1 * b1.y, a1 * b1.x, -a2 * b2.x);
+    const Vector b3r3(0.0, -b3.z, b3.y);
+    const Vector b3_plus_r3(b3.x - 1.0, b3.y, b3.z);
+    const float b3_dot_r3_plus_1 = 1.0 - b3.x;
   
-    float a1 = 1.0, a2 = 2.0;
-    const Vector b1r1a1_plus_b2r2a2 = b1 * r1 * a1 + b2 * r2 * a2;
-    const Vector b3r3 = b3 * r3;
-    const Vector b3_plus_r3 = b3 + r3;
-    const float b3_dot_r3_plus_1 = 1.0 + b3 % r3;
-  
-    const float alpha = b3_dot_r3_plus_1 * (b1 % r1 * a1 + b2 % r2 * a2) + b3r3 % b1r1a1_plus_b2r2a2;
+    const float alpha = -b3_dot_r3_plus_1 * (a1 * b1.z + a2 * b2.y) + b3r3 % b1r1a1_plus_b2r2a2;
     const float beta = b3_plus_r3 % b1r1a1_plus_b2r2a2;
     const float gamma = sqrt(alpha * alpha + beta * beta);
   
@@ -38,3 +45,43 @@ void Attitude::EstimateTask::run() {
   app.serial.debug("roll", result.roll());
   app.serial.line();
 }
+// #include "attitude.h"
+// #include "app.h"
+// #include "timed_section.h"
+// #include "quaternion.h"
+// #include "serial.h"
+// 
+// void Attitude::EstimateTask::run() {
+//   app.accelerometer.measure(true);
+//   app.magnetometer.measure(true);
+//   
+//   do {
+//     TimedSection ts("estimate");
+//     const Vector b1 = app.accelerometer.vector().normalised(); // gravity
+//     const Vector b2 = (b1 * app.magnetometer.vector()).normalised(); // magnetic west
+//     const Vector b3 = b1 * b2;
+//   
+//     float a1 = 1.0, a2 = 2.0;
+//     const Vector b1r1a1_plus_b2r2a2 = b1 * r1 * a1 + b2 * r2 * a2;
+//     const Vector b3r3 = b3 * r3;
+//     const Vector b3_plus_r3 = b3 + r3;
+//     const float b3_dot_r3_plus_1 = 1.0 + b3 % r3;
+//   
+//     const float alpha = b3_dot_r3_plus_1 * (b1 % r1 * a1 + b2 % r2 * a2) + b3r3 % b1r1a1_plus_b2r2a2;
+//     const float beta = b3_plus_r3 % b1r1a1_plus_b2r2a2;
+//     const float gamma = sqrt(alpha * alpha + beta * beta);
+//   
+//     if (alpha >= 0) {
+//       result = Quaternion(b3r3 * (gamma + alpha) + b3_plus_r3 * beta, (gamma + alpha) * b3_dot_r3_plus_1);
+//       result /= 2 * sqrt(gamma * (gamma + alpha) * b3_dot_r3_plus_1);
+//     } else {
+//       result = Quaternion(b3r3 * beta + b3_plus_r3 * (gamma - alpha), beta * b3_dot_r3_plus_1);
+//       result /= 2 * sqrt(gamma * (gamma - alpha) * b3_dot_r3_plus_1);
+//     }
+//   } while (false);
+//   
+//   app.serial.debug("yaw", result.yaw());
+//   app.serial.debug("pitch", result.pitch());
+//   app.serial.debug("roll", result.roll());
+//   app.serial.line();
+// }
--- attitude.h
+++ attitude.h
@@ -15,11 +15,11 @@ public:
     
   class EstimateTask : public Scheduler::Task {
   private:
-    const Vector r1, r2, r3;
+    const float a1, a2; // TODO: normalise these weights around a1?
   public:
     Quaternion result;
 
-    EstimateTask() : Scheduler::Task(20), r1(0.0, 0.0, -1.0), r2(0.0, -1.0, 0.0), r3(r1 * r2) { }
+    EstimateTask() : Scheduler::Task(20), a1(1.0), a2 (2.0) { }
     
     void run();
   } estimate;
@@ -27,3 +27,34 @@ public:
 };
 
 #endif
+
+
+// #ifndef __ATTITUDE_H_
+// #define __ATTITUDE_H_
+// // 
+// // class App;
+// // extern App app;
+// 
+// #include "magnetometer.h"
+// #include "accelerometer.h"
+// #include "quaternion.h"
+// 
+// class Attitude {
+// public:
+//   Attitude() { }
+//   void init() { }
+//     
+//   class EstimateTask : public Scheduler::Task {
+//   private:
+//     const Vector r1, r2, r3;
+//   public:
+//     Quaternion result;
+// 
+//     EstimateTask() : Scheduler::Task(20), r1(0.0, 0.0, -1.0), r2(0.0, -1.0, 0.0), r3(r1 * r2) { }
+//     
+//     void run();
+//   } estimate;
+//   
+// };
+// 
+// #endif
