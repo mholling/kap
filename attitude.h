ebbeddfa6927352fde7644cbbeff98216db15094
Matthew Hollingworth <mdholling@gmail.com>
2010-02-02 10:59:08 +1100

    tidyup

--- attitude.h
+++ attitude.h
@@ -1,8 +1,5 @@
 #ifndef __ATTITUDE_H_
 #define __ATTITUDE_H_
-// 
-// class App;
-// extern App app;
 
 #include "magnetometer.h"
 #include "accelerometer.h"
@@ -27,34 +24,3 @@ public:
 };
 
 #endif
-
-
-// #ifndef __ATTITUDE_H_
-// #define __ATTITUDE_H_
-// // 
-// // class App;
-// // extern App app;
-// 
-// #include "magnetometer.h"
-// #include "accelerometer.h"
-// #include "quaternion.h"
-// 
-// class Attitude {
-// public:
-//   Attitude() { }
-//   void init() { }
-//     
-//   class EstimateTask : public Scheduler::Task {
-//   private:
-//     const Vector r1, r2, r3;
-//   public:
-//     Quaternion result;
-// 
-//     EstimateTask() : Scheduler::Task(20), r1(0.0, 0.0, -1.0), r2(0.0, -1.0, 0.0), r3(r1 * r2) { }
-//     
-//     void run();
-//   } estimate;
-//   
-// };
-// 
-// #endif
