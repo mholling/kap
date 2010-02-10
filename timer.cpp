234e1475701c2cdbd061da89421d2966861956e1
Matthew Hollingworth <mdholling@gmail.com>
2010-02-11 08:46:25 +1100

    removed timed_section class and added global timestamp and a timing diagnostic class

--- timer.cpp
+++ timer.cpp
@@ -2,21 +2,31 @@
 #include <avr/io.h>
 #include "app.h"
 
-Timer::Timer() {
+Timer::Timer() : count(0) {
   TCCR2A = _BV(WGM21); // CTC mode
   TCCR2B = _BV(CS22) | _BV(CS21) | _BV(CS20);
   OCR2A = F_CPU / 1024 / frequency - 1;
   TIMSK2 = _BV(OCIE2A); 
 }
 
+unsigned long int Timer::timestamp() {
+  return count * OCR2A + TCNT2;
+}
+
 void Timer::interrupt() {
-  static unsigned int count = 0;
   count++;
   
   if (count % (frequency / 10) == 0) app.diagnostic();
   
   app.magnetometer.measure();
   app.magnetometer.calibrate();
+}
+
+Timer::Diagnostic::Diagnostic(const char *message) : start(app.timer.timestamp()), message(message) { }
+
+Timer::Diagnostic::~Diagnostic() {
+  const unsigned long int finish = app.timer.timestamp();
+  app.serial.debug(message, static_cast<float>(finish - start) / (OCR2A * Timer::frequency));
 }
 
 ISR(TIMER2_COMPA_vect) {
--- timer.h
+++ timer.h
@@ -2,11 +2,24 @@
 #define __TIMER_H_
 
 class Timer {
+private:
+  unsigned long int count;
+  
 public:
   enum { frequency = 35 };
   Timer();
   inline void init() { }
   void interrupt();
+  unsigned long int timestamp();
+  
+  class Diagnostic {
+  private:
+    const unsigned long int start;
+    const char *message;
+  public:
+    Diagnostic(const char *message);
+    ~Diagnostic();
+  };
 };
 
 #endif
