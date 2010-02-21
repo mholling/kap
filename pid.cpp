4b2b80176298d6cd89a96e6020d8261829e70a8e
Matthew Hollingworth <mdholling@gmail.com>
2010-02-21 17:46:52 +1100

    added integral term clamping to prevent windup in PID:

--- pid.cpp
+++ pid.cpp
@@ -4,18 +4,17 @@
 Pid::Pid() : yaw(app.gyros.yaw, app.trajectory.yaw, app.motors.yaw), pitch(app.gyros.pitch, app.trajectory.pitch, app.motors.pitch) { }
 
 void Pid::Controller::run() {
-  state.update();
-  motor.set(kp * state.error + ki * state.integral + kd * state.derivative);
-}
-
-void Pid::Controller::State::reset() {
-  error = derivative = integral = 0.0;
-  previous = trajectory_angle();
-}
-
-void Pid::Controller::State::update() {
-  error = trajectory_angle() - gyro.estimate.angle();
-  derivative = (trajectory_angle() - previous) * Timer::frequency - gyro.estimate.rate();
+  float error = target() - gyro.estimate.angle();
+  float derivative = (target() - previous) * Timer::frequency - gyro.estimate.rate();
   integral += error / Timer::frequency;
-  previous = trajectory_angle();
+  if (integral >  clamp) integral =  clamp;
+  if (integral < -clamp) integral = -clamp;
+  previous = target();
+  
+  motor.set(kp * (error + integral / ti + derivative * td));
+}
+
+void Pid::Controller::reset() {
+  integral = 0.0;
+  previous = target();
 }
--- pid.h
+++ pid.h
@@ -13,31 +13,30 @@ public:
   
   class Controller : public Timer::Task {
   private:
-    class State {
-    private:
-      const Gyros::Gyro& gyro;
-      const Trajectory::Angle trajectory_angle;
-      float previous;
-      
-    public:
-      State(const Gyros::Gyro& gyro, const Trajectory::Angle trajectory_angle) : gyro(gyro), trajectory_angle(trajectory_angle) { reset(); }
-      
-      void reset();
-      void update();
-      
-      float error;
-      float derivative;
-      float integral;
-    } state;
-    
+    const Gyros::Gyro& gyro;
+    const Trajectory::Angle target;
     Motors::Motor& motor;
-    const float kp, ki, kd;
-  
+
+    float previous;
+    float integral;
+      
+    const float kp;
+    const float ti;
+    const float td;
+    const float clamp;
+    
   public:
-    Controller(const Gyros::Gyro& gyro, const Trajectory::Angle trajectory_angle, Motors::Motor& motor) : state(gyro, trajectory_angle), motor(motor), kp(1.0), ki(0.2), kd(0.0) { }
+    Controller(const Gyros::Gyro& gyro, const Trajectory::Angle target, Motors::Motor& motor) :
+      gyro(gyro),
+      target(target),
+      motor(motor),
+      kp(1.0), ti(0.2), td(0.0), clamp(ti / kp) { reset(); }
     void init() { }
     
     void run();
+
+    void reset();
+    void update();
   };
   
   Controller yaw;
@@ -45,3 +44,68 @@ public:
 };
 
 #endif
+
+
+
+
+
+
+// #ifndef __PID_H_
+// #define __PID_H_
+// 
+// #include "scheduler.h"
+// #include "gyros.h"
+// #include "motors.h"
+// #include "trajectory.h"
+// 
+// class Pid {
+// public:
+//   Pid();
+//   void init() { yaw.init(); pitch.init(); }
+//   
+//   class Controller : public Timer::Task {
+//   private:
+//     class State {
+//     private:
+//       const Gyros::Gyro& gyro;
+//       const Trajectory::Angle trajectory_angle;
+//       float previous;
+//       
+//     public:
+//       State(const Gyros::Gyro& gyro, const Trajectory::Angle trajectory_angle) : gyro(gyro), trajectory_angle(trajectory_angle) { reset(); }
+//       
+//       void reset();
+//       void update();
+//       
+//       float error;
+//       float derivative;
+//       float integral;
+//     } state;
+//     
+//     class Gains {
+//     private:
+//       const State& state;
+//       float kp;
+//       float ti;
+//       float td;
+//       
+//     public:
+//       Gains(const State& state) : state(state) { }
+//       
+//     } gains;
+//     
+//     Motors::Motor& motor;
+//     const float kp, ki, kd;
+//   
+//   public:
+//     Controller(const Gyros::Gyro& gyro, const Trajectory::Angle trajectory_angle, Motors::Motor& motor) : state(gyro, trajectory_angle), gains(state), motor(motor), kp(1.0), ki(0.2), kd(0.0) { }
+//     void init() { }
+//     
+//     void run();
+//   };
+//   
+//   Controller yaw;
+//   Controller pitch;
+// };
+// 
+// #endif
